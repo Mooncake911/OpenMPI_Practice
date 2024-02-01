b3ea139e08955c945a1e6be86a8c71ffc1f47026
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2024-02-01 20:35:59 +0300

    First commit

--- doExperiments.py
+++ doExperiments.py
@@ -18,7 +18,8 @@ class DoExperiments:
                  from_n: int = 1, to_n: int = 16,
                  from_iters: int = 10, to_iters: int = 1000000,
                  iter_step: float = 10, step_mode: str = "*",
-                 args_in_right_order=tuple()):
+                 args_in_right_order=tuple(),
+                 columns_names: list = None):
 
         self.path = exe_path
         self.from_n = min(int(from_n), int(to_n))
@@ -31,6 +32,7 @@ class DoExperiments:
 
         self.my_args = args_in_right_order
         self.data_list = []
+        self.columns = columns_names
 
         self.rise_error()
 
@@ -47,6 +49,10 @@ class DoExperiments:
             s *= self.iter_step
         return s
 
+    def add_columns(self, n):
+        while len(self.columns) < n:
+            self.columns.append(str(len(self.columns)))
+
     def start_open_mpi(self, n, *args):
         """ C++ code to start in commandline interface """
         command = f"mpiexec -n {n} {self.path} {' '.join(map(str, args))}"
@@ -58,7 +64,7 @@ class DoExperiments:
             return output
         elif stderr:
             print(f"[::COMMAND RISE ERROR]\n {command} =>", Exception(stderr.decode()))
-            return
+            return [None]
 
     def run(self, filename):
         """ Run the experiments with OpenMPI """
@@ -69,8 +75,9 @@ class DoExperiments:
                 self.data_list.append([n, iters, *output])
             iters = int(self.make_step(iters))
 
+        self.add_columns(len(self.data_list[0]))
         with open(filename, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(['Num_Threads', 'Iter', 'Time'])
+            csv_writer.writerow(self.columns)
             csv_writer.writerows(self.data_list)
         print(f"Results saved to {filename}")
--- myplots.py
+++ myplots.py
@@ -2,79 +2,92 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def time_iter_plot(thread_groups, labels, size=(15, 15),
-                   title: str = 'Dependency of Iter on Time for Different Num_Threads'):
-    plt.figure(figsize=size)
-    plt.subplots_adjust(hspace=0.5, wspace=0.5)
-    plt.suptitle(title, fontsize=30)
+class Plots:
+    def __init__(self, tables, num_threads=str, iters=str, time=str):
+        self.num_threads = str(num_threads)
+        self.iters = str(iters)
+        self.time = str(time)
 
-    for group, label in zip(thread_groups, labels):
-        s = np.ceil(np.sqrt(len(group))).astype(int)
-        for index, (thread_count, group_data) in enumerate(group, start=1):
-            plt.subplot(s, s, index)
-            plt.plot(group_data['Iter'], group_data['Time'],
-                     linestyle='--', marker='o',
-                     label=f'{label} - Num_Threads = {thread_count}')
+        self.tables = tables
+        self.thread_groups = []
+        self.iter_groups = []
 
-            plt.xlabel('Iter')
-            plt.ylabel('Time')
-            plt.legend()
+        for t in self.tables.values():
+            self.thread_groups.append(t.groupby(self.num_threads, as_index=False))
+            self.iter_groups.append(t.groupby(self.iters, as_index=False))
 
-    plt.show()
+    def time_iter_plot(self, size=(15, 15)):
+        title = f'Dependency of {self.iters} on {self.time} for Different {self.num_threads}'
+        plt.figure(figsize=size)
+        plt.subplots_adjust(hspace=0.5, wspace=0.5)
+        plt.suptitle(title, fontsize=30)
 
+        for group, label in zip(self.thread_groups, self.tables.keys()):
+            s = np.ceil(np.sqrt(len(group))).astype(int)
+            for index, (thread_count, group_data) in enumerate(group, start=1):
+                plt.subplot(s, s, index)
+                plt.plot(group_data[self.iters], group_data[self.time],
+                         linestyle='--', marker='o',
+                         label=f'{label} - {self.num_threads} = {thread_count}')
 
-def time_thread_plot(iter_groups, labels, size=(15, 15),
-                     title: str = 'Dependency of Num_Threads on Time for Different Iter'):
-    plt.figure(figsize=size)
-    plt.subplots_adjust(hspace=0.5, wspace=0.5)
-    plt.suptitle(title, fontsize=30)
+                plt.xlabel(self.iters)
+                plt.ylabel(self.time)
+                plt.legend()
 
-    for group, label in zip(iter_groups, labels):
-        for index, (iter_value, group_data) in enumerate(group, start=1):
-            group_data = group_data.reset_index()
+        plt.show()
 
-            plt.subplot(len(group), 1, index)
-            plt.plot(group_data['Num_Threads'], group_data['Time'],
-                     marker='o', linestyle='-',
-                     label=f'{label} - Iter = {iter_value}')
+    def time_thread_plot(self, size=(15, 15)):
+        title = f'Dependency of {self.num_threads} on {self.time} for Different {self.iters}'
+        plt.figure(figsize=size)
+        plt.subplots_adjust(hspace=0.5, wspace=0.5)
+        plt.suptitle(title, fontsize=30)
 
-            # Выделение точки с наименьшим значением красным цветом
-            min_time_idx = group_data['Time'].idxmin()
-            plt.scatter(group_data['Num_Threads'].iloc[min_time_idx], group_data['Time'].iloc[min_time_idx],
-                        color='black', marker='X', zorder=10)
+        for group, label in zip(self.iter_groups, self.tables.keys()):
+            for index, (iter_value, group_data) in enumerate(group, start=1):
+                group_data = group_data.reset_index()
 
-            plt.xlabel('Num_Threads')
-            plt.ylabel('Time')
-            plt.legend()
+                plt.subplot(len(group), 1, index)
+                plt.plot(group_data[self.num_threads], group_data[self.time],
+                         marker='o', linestyle='-',
+                         label=f'{label} - {self.iters} = {iter_value}')
 
-    plt.show()
+                # Выделение точки с наименьшим значением красным цветом
+                min_time_idx = group_data[self.time].idxmin()
+                plt.scatter(group_data[self.num_threads].iloc[min_time_idx], group_data[self.time].iloc[min_time_idx],
+                            color='black', marker='X', zorder=10)
 
+                plt.xlabel(self.num_threads)
+                plt.ylabel(self.time)
+                plt.legend()
 
-def speedup_plot(iter_groups, labels, base_num_threads=1, size=(15, 15),
-                 title: str = 'Dependency of Num_Threads on Speedup for Different Iter'):
-    plt.figure(figsize=size)
-    plt.subplots_adjust(hspace=0.5, wspace=0.5)
-    plt.suptitle(title, fontsize=30)
+        plt.show()
 
-    for group, label in zip(iter_groups, labels):
-        for index, (iter_value, group_data) in enumerate(group, start=1):
-            group_data = group_data.reset_index()
+    def speedup_plot(self, base_num_threads=1, size=(15, 15)):
+        title = f'Dependency of {self.num_threads} on Speedup for Different {self.iters}'
+        plt.figure(figsize=size)
+        plt.subplots_adjust(hspace=0.5, wspace=0.5)
+        plt.suptitle(title, fontsize=30)
 
-            base_time = group_data[group_data['Num_Threads'] == base_num_threads]['Time'].values[0]
-            speedup = base_time / group_data['Time']
+        for group, label in zip(self.iter_groups, self.tables.keys()):
+            for index, (iter_value, group_data) in enumerate(group, start=1):
+                group_data = group_data.reset_index()
 
-            plt.subplot(len(group), 1, index)
-            plt.plot(group_data['Num_Threads'], speedup,
-                     marker='o', linestyle='-',
-                     label=f'{label} - Iter = {iter_value}')
+                base_time = group_data[group_data[self.num_threads] == base_num_threads][self.time].values[0]
+                speedup = base_time / group_data[self.time]
 
-            # Выделение точки с наибольшим значением ускорения красным цветом
-            max_speedup_idx = np.argmax(speedup)
-            plt.scatter(group_data['Num_Threads'].iloc[max_speedup_idx], speedup.iloc[max_speedup_idx], color='black',
-                        marker='X', zorder=10)
+                plt.subplot(len(group), 1, index)
+                plt.plot(group_data[self.num_threads], speedup,
+                         marker='o', linestyle='-',
+                         label=f'{label} - {self.iters} = {iter_value}')
 
-            plt.xlabel('Num_Threads')
-            plt.ylabel('Speedup')
-            plt.legend()
+                # Выделение точки с наибольшим значением ускорения красным цветом
+                max_speedup_idx = np.argmax(speedup)
+                plt.scatter(group_data[self.num_threads].iloc[max_speedup_idx], speedup.iloc[max_speedup_idx],
+                            color='black',
+                            marker='X', zorder=10)
 
-    plt.show()
+                plt.xlabel(self.num_threads)
+                plt.ylabel('Speedup')
+                plt.legend()
+
+        plt.show()
